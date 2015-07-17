78a9c3ac2ed019f2e423886fcb2e72bd65fcb5ad
stuckata <stu.kirkov@gmail.com>
2015-07-17 18:09:23 +0300

    problem 01

---
+++ 01-Entity-Framework/Homework/01_SoftUniDatabase/01_SoftUniDatabase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SoftUniDatabase
+{
+    class Program
+    {
+        static void Main()
+        {
+            var context = new SoftUniEntities();
+            var employees = context.Employees;
+        }
+    }
+}
