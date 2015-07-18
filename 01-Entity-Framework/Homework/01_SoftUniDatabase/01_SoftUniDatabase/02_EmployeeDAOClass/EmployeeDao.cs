03d2042c0ff77b62b5fd226108742e2948a98995
stuckata <stu.kirkov@gmail.com>
2015-07-18 21:00:50 +0300

    problem 02

---
+++ 01-Entity-Framework/Homework/01_SoftUniDatabase/01_SoftUniDatabase/02_EmployeeDAOClass/EmployeeDao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SoftUniDatabase
+{
+    public class EmployeeDao
+    {
+
+        public static void Add(Employee employee)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                context.Employees.Add(employee);
+                context.SaveChanges();
+            }
+        }
+
+        public static Employee FindByKey(int key)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                return context.Employees.Find(key);
+            }
+        }
+
+        public static void Modify(Employee employee, string newFirstName)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                var currentEmployee = context.Employees.Find(employee.EmployeeID);
+                currentEmployee.FirstName = newFirstName;
+                context.SaveChanges();
+            }         
+        }
+
+        public static void Delete(Employee employee)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                context.Employees.Remove(employee);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- 01-Entity-Framework/Homework/01_SoftUniDatabase/01_SoftUniDatabase/Program.cs
+++ 01-Entity-Framework/Homework/01_SoftUniDatabase/01_SoftUniDatabase/Program.cs
@@ -10,8 +10,26 @@ namespace _01_SoftUniDatabase
     {
         static void Main()
         {
-            var context = new SoftUniEntities();
-            var employees = context.Employees;
+            var gandolf = new Employee()
+            {
+                FirstName = "Gandolf",
+                LastName = "Thegray",
+                JobTitle = "Network Administrator",
+                DepartmentID = 11,
+                HireDate = new DateTime(2001, 03, 27),
+                Salary = 32500
+            };
+
+            //EmployeeDao.Add(gandolf);
+
+            var currentEmployee = EmployeeDao.FindByKey(294);
+            Console.WriteLine(currentEmployee.FirstName);
+
+            //EmployeeDao.Modify(gandolf, "Gandalf");
+            //Console.WriteLine(gandolf.FirstName);
+
+            //EmployeeDao.Delete(gandolf);
+
         }
     }
 }
