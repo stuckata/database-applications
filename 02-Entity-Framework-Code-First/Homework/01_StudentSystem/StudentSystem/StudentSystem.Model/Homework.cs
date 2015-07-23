138cdb25baeb37c08c7afbb62aa155f3709ebf07
stuckata <stu.kirkov@gmail.com>
2015-07-23 16:09:12 +0300

    problem 01 homework

---
+++ 02-Entity-Framework-Code-First/Homework/01_StudentSystem/StudentSystem/StudentSystem.Model/Homework.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Mime;
+
+namespace StudentSystem.Model
+{
+    public class Homework
+    {
+        public int Id { get; set; }
+
+        public ContentType ContentType { get; set; }
+
+        public DateTime SubmissionDate { get; set; }
+    }
+}
