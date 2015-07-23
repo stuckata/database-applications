fe1b48b00b2e860c0d8d38be36b8eb47a90e1d9b
stuckata <stu.kirkov@gmail.com>
2015-07-23 14:23:30 +0300

    problem 01 Course

---
+++ 02-Entity-Framework-Code-First/Homework/01_StudentSystem/StudentSystem/StudentSystem.Model/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentSystem.Model
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ 02-Entity-Framework-Code-First/Homework/01_StudentSystem/StudentSystem/StudentSystem.Model/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentSystem.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public int Name { get; set; }
+
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime? Birthday { get; set; }
+    }
+}
