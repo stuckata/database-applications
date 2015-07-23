ed5f66e0fd4d48fadbf3c7e2da3fa5d0f1e8966d
stuckata <stu.kirkov@gmail.com>
2015-07-23 14:31:26 +0300

    problem 01 resource

---
+++ 02-Entity-Framework-Code-First/Homework/01_StudentSystem/StudentSystem/StudentSystem.Model/Resource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentSystem.Model
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        public Uri Url { get; set; }
+    }
+}
