a90d730ba49dcd61ba3b3533da8b083c3e00b976
stuckata <stu.kirkov@gmail.com>
2015-08-01 13:53:23 +0300

    problem 02 ready

--- 10-Database-Apps-Exam-Football/DbAppsExamPreparation/DbAppsExamPreparation/02-ExportLeaguesAndTeamsAsJSON/ExportLeaguesAndTeamsAsJSON.cs
+++ 10-Database-Apps-Exam-Football/DbAppsExamPreparation/DbAppsExamPreparation/02-ExportLeaguesAndTeamsAsJSON/ExportLeaguesAndTeamsAsJSON.cs
@@ -1,9 +1,6 @@
 ﻿using _01_DatabaseFirst;
-using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 
 namespace _02_ExportLeaguesAndTeamsAsJSON
@@ -14,16 +11,16 @@ namespace _02_ExportLeaguesAndTeamsAsJSON
         {
             var context = new FootballEntities();
 
-            var leagues = context.Leagues.Select(l => new
+            var leagues = context.Leagues.OrderBy(l => l.LeagueName).Select(l => new
                 {
                     leagueName = l.LeagueName,
-                    teams = l.Teams.Select(t => t.TeamName)
+                    teams = l.Teams.OrderBy(t => t.TeamName).Select(t => t.TeamName)
                 }
             );
 
             var json = new JavaScriptSerializer().Serialize(leagues);
 
-            Console.WriteLine(json);
+            File.WriteAllText(@"../../leagues-and-teams.json", json);
         }
     }
 }
